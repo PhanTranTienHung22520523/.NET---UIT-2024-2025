1934cbb6b458d220efa0d2affe6d81f0d0bd298f
NgoDuyHung2305 <22520511@gm.uit.edu.vn>
2024-11-11 16:02:20 +0700

    UI(AllRoomView)

---
+++ Apartment_Management_.NET/Apartment_Management/View/AllRoomView.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Apartment_Management.View
+{
+    /// <summary>
+    /// Interaction logic for AllRoomView.xaml
+    /// </summary>
+    public partial class AllRoomView : UserControl
+    {
+        public ObservableCollection<string> Floors { get; set; }
+        public ObservableCollection<Room> RoomList { get; set; }
+
+        public AllRoomView()
+        {
+            InitializeComponent();
+            Floors = new ObservableCollection<string>
+        {
+            "Tầng 1",
+            "Tầng 2",
+            "Tầng 3"
+        };
+            RoomList = new ObservableCollection<Room>{
+            new Room { Name = "101", HostName = "Hưng Ngô", Price = 5000000  },
+            new Room { Name = "102", HostName = "Hưng Tiến", Price = 10000000  },
+        };
+            DataContext = this;
+        }
+    }
+    public class Room
+    {
+        public string Name { get; set; }
+        public string HostName { get; set; }
+        public int Price { get; set; }
+    }
+}
